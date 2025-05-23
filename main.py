1d7fe9a57f48ccd8b67aec3bed0138169200f7ae
AdamHelal <helal.adam1234@gmail.com>
2025-05-23 16:57:40 +1000

    removed unused turtle class

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ Created on May 14 2025:
 This will be the main file to run the simulation. This should include the setup and the go functionality.
 """
 from numpy.f2py.auxfuncs import throw_error
-from turtle import Turtle
 from Agent import Agent
 from Cop import Cop
 from Patch import Patch
